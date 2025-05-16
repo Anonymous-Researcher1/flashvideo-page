0aa838df85711792ea2acb675294b53e9ce2563a
Anonymous-Researcher1 <2017302580107@whu.edu.cn>
2025-05-16 12:01:39 +0800

    add cmp

---
+++ static/githubfigs/method_cmp/first_frame.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+
+video_dir = './'
+
+for filename in os.listdir(video_dir):
+    if filename.endswith('.mp4'):
+        video_path = os.path.join(video_dir, filename)
+        img_path = os.path.join(video_dir, filename.rsplit('.', 1)[0] + '.jpg')
+        
+        cap = cv2.VideoCapture(video_path)
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite(img_path, frame)
+            print(f'Saved first frame of {filename} as {img_path}')
+        else:
+            print(f'Failed to read {filename}')
+        cap.release()
